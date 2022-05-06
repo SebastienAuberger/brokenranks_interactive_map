7e04e9e040a28da0526889dbadcca2545055e237
Sébastien Auberger <sebastien.abg@gmail.com>
2022-05-06 22:12:22 +0200

    Fix: Filter Menu can now open and close

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,23 @@ map.initMap();
 var hideButton = document.getElementById('hide-button');
 var filterMenu = document.getElementById('filter-menu');
 var MapContainer = document.getElementById('map');
-
+var hideButtonSize = hideButton.offsetWidth
+var toggleHideButton = true;
 hideButton.addEventListener('click',() => {
-    filterMenu.style.width = 0;
-    MapContainer.style.width = '100%';
+    if(toggleHideButton)
+    {
+        filterMenu.style.width = 0;
+        MapContainer.style.width = '100%';
+        hideButton.style.left = '39px';
+        hideButton.innerHTML = ">>";
+        toggleHideButton = false;
+    }
+    else
+    {
+        filterMenu.style.width = '10%';
+        MapContainer.style.width = '90%';
+        hideButton.style.left = '0px';
+        hideButton.innerHTML = "<<";
+        toggleHideButton = true;
+    }
 })
