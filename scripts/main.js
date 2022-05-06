3b7bfb07887f07085f4ccd8697fdcadd1ad510f9
Sébastien Auberger <sebastien.abg@gmail.com>
2022-05-06 20:37:57 +0200

    Feat: Add side filter menu (WIP)

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 var map = new BRMap();
 map.initMap();
+
+var hideButton = document.getElementById('hide-button');
+var filterMenu = document.getElementById('filter-menu');
+var MapContainer = document.getElementById('map');
+
+hideButton.addEventListener('click',() => {
+    filterMenu.style.width = 0;
+    MapContainer.style.width = '100%';
+})
