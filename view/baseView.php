6ecc0d2ee53bdc01bd71b4b1f1a1d18acbd394a9
Sébastien Auberger <sebastien.abg@gmail.com>
2022-05-09 21:28:27 +0200

    Fix: PHP error when user go on BaseView directly

--- view/baseView.php
+++ view/baseView.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
-    <title><?php echo $title ?></title>
+    <title><?php echo isset($title) ? $title : "Undefined title" ?></title>
     <link rel="stylesheet" href="leaflet/leaflet.css"/>
     <link rel="stylesheet" href="css/style.css">
     <script src="leaflet/leaflet.js"></script>
     <script src="scripts/brmap.class.js"></script>
 </head>
 <body>
-    <?php echo $content ?>
+    <?php echo isset($content) ? $content : "Aucun contenu à afficher" ?>
 </body>
