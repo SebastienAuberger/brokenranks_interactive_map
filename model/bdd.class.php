cb7518f60f67fb63016b4e85743e3f7264f26a75
Sébastien Auberger <sebastien.abg@gmail.com>
2022-05-12 16:48:18 +0200

    Feat: Adding addMarker()

--- model/bdd.class.php
+++ model/bdd.class.php
@@ -10,6 +10,7 @@
             $this->username = $username;
             try{
                 $this->bdd = new PDO('mysql:host=' . $this->host . ';dbname=int_map',$this->username,$this->password);
+                $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch(PDOException $e){
                 echo "Problème lors de la connexion à la base de données -> " . $e->getMessage();
@@ -17,8 +18,30 @@
         }
 
         function getMarkers(){
-            $req = $this->bdd->prepare("SELECT * FROM `markers`");
-            $req->execute();
-            return $req;
+            try{
+                $req = $this->bdd->prepare("SELECT * FROM `markers`");
+                $req->execute();
+                return $req->fetchAll();
+            }catch(PDOException $e){
+                echo("ERREUR:" . $e->getMessage());
+            }
+        }
+
+        function addMarker($posX,$posY,$popup = "0",$html = ""){
+            if(($posX > -200 && $posX < 200) && ($posY > -200 && $posY < 200)){
+                try{
+                    $req = $this->bdd->prepare("INSERT INTO `markers`(id,posX,posY,popup,html) VALUES (NULL,:posX,:posY,:popup,:html)");
+                    $req->bindParam(':posX',$posX);
+                    $req->bindParam(':posY',$posY);
+                    $req->bindParam(':popup',$popup);
+                    $req->bindParam(':html',$html);
+                    $req->execute();
+                }catch(PDOException $e){
+                    echo("ERREUR: " . $e->getMessage());
+                }
+            }
+            else{
+                throw new RangeException("POSX or POSY is out of Range");
+            }
         }
     }
--- model/getMarkers.php
+++ model/getMarkers.php
@@ -3,5 +3,5 @@
 include("../model/bdd.class.php");
 header("Content-type: application/json");
 $bddManager = new BDD("root","","127.0.0.1");
-$markers = $bddManager->getMarkers()->fetchAll();
+$markers = $bddManager->getMarkers();
 echo(json_encode($markers));
